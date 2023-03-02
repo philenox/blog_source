6000e31b86530482ffca88d50cc7f085fb08dbf0
Phil Lenox <philiplenox@comcast.net>
2023-03-02 15:48:23 +0000

    simple captcha implemented. Removed comment e-mail

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,14 @@
 from .models import Comment, EmailSubscription
 from django import forms
+from captcha.fields import CaptchaField
 
 
 class CommentForm(forms.ModelForm):
     body = forms.CharField(widget=forms.Textarea(attrs={'cols': 20, 'rows': 10}))
+    captcha = CaptchaField()
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'body')
+        fields = ('name', 'body')
 
 class EmailForm(forms.ModelForm):
     class Meta:
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('blog.urls')),
 ]
