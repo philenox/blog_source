0400cab080cd96b4534df9c52f37b2354f3f36ca
Philenox <philiplenox@comcast.net>
2024-11-16 13:22:01 +0000

    add admin site back in

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path, include
 from blog import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),  # Admin site
     path('', views.PostList.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('subscribe/', views.subscribe, name='subscribe'),
