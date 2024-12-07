9a8d2253c22d2b3459f4f548ac78c7899ff422d1
Philenox <philiplenox@comcast.net>
2024-12-07 14:53:35 +0000

    Add sortable table by tags to site

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Recipes
+from .models import Recipes, Tags
 
 def recipe_index(request):
-    recipes = Recipes.objects.using('recipes_db').all()
-    return render(request, 'recipe_index.html', {'recipes': recipes})
+    selected_tags = request.GET.getlist('tags')
+    tags = Tags.objects.using('recipes_db').all()
+    
+    if selected_tags:
+        recipes = Recipes.objects.using('recipes_db').filter(
+            recipe_tags__tag_id__in=selected_tags
+        ).distinct()
+    else:
+        recipes = Recipes.objects.using('recipes_db').all()
+    
+    return render(request, 'recipe_index.html', {
+        'recipes': recipes,
+        'tags': tags,
+        'selected_tags': list(map(int, selected_tags))
+    })
 
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipes.objects.using('recipes_db'), recipe_id=recipe_id)
