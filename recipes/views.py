d22805375999c4ad7ffdb9a62311366177554653
Philenox <philiplenox@comcast.net>
2024-12-07 16:00:38 +0000

    added images

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from .models import Recipes, Tags
 
@@ -22,14 +24,28 @@ def recipe_index(request):
         'selected_tags': selected_tags,
     })
 
+
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipes.objects.using('recipes_db'), recipe_id=recipe_id)
     instructions = recipe.instructions.all()
     ingredients = recipe.recipe_ingredients.all()
     tags = recipe.recipe_tags.all()
+    
+    # Determine the image path
+    recipe_image_name = f"{recipe.title}.png"
+    image_path = os.path.join(settings.MEDIA_ROOT, 'image', recipe_image_name)
+    
+    if os.path.exists(image_path):
+        # If the image exists, use its URL
+        recipe_image_url = os.path.join(settings.MEDIA_URL, 'image', recipe_image_name)
+    else:
+        # Otherwise, use the default image
+        recipe_image_url = os.path.join(settings.MEDIA_URL, 'image', 'default_recipe_photo.webp')
+    
     return render(request, 'recipe_detail.html', {
         'recipe': recipe,
         'instructions': instructions,
         'ingredients': ingredients,
-        'tags': tags
+        'tags': tags,
+        'recipe_image_url': recipe_image_url,  # Pass the image URL to the template
     })
